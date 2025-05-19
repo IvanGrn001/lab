26db2a827e0d8c6e6f2e4edcebc69cba18c5ea4c
IvanGrn001 <grebenyuk.ivan.mobile@gmail.com>
2025-05-19 14:16:35 +0300

    Create lab.java

---
+++ lab.java
@@ -0,0 +1,9 @@
+public class StudentSubjectInfo {
+    public static void main(String[] args) {
+        String surname = "Grebenyuk";           
+        String subject = "TRPO"; 
+
+        System.out.println("Surname: " + surname);
+        System.out.println("Subject: " + subject);
+    }
+}
